363a10beb005a1aabc55b88caebcbb49fb3b5b8d
jcilwz <63843752@qq.com>
2017-11-13 18:50:27 +0800

    实验6-10

---
+++ CProgramming/ex6_10.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+
+float fun(double h)
+{
+	int x;
+	x = (int)(h*1000);
+	if(x%10>=5) 
+		x=x/10+1;
+	else 
+		x=x/10;
+	return x/(double)100;
+}
+void main()
+{
+	double h;
+	cin>>h;
+	cout<<fun(h)<<endl;
+}
